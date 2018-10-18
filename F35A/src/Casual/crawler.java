d6b03fcd645eabf894b6b2920d93c0dafd1e514d
chaokuang <chao_kuang@apple.com>
2018-10-18 14:20:52 +0800

    grab something

--- F35A/src/Casual/crawler.java
+++ F35A/src/Casual/crawler.java
@@ -1,7 +1,77 @@
 package Casual;
 
 public class crawler {
-	
+	package spider;
+
+	import domain.Zhihu;
+
+	import java.io.BufferedReader;
+	import java.io.InputStreamReader;
+	import java.net.URL;
+	import java.net.URLConnection;
+	import java.util.ArrayList;
+	import java.util.regex.Matcher;
+	import java.util.regex.Pattern;
+
+	/**
+	 * 1.获取网页源码
+	 * 2.将与正则表达式匹配的字符串封装到Zhihu对象中，然后将Zhihu对象添加至泛型ArrayList中。
+	 * Created by codingBoy on 16/10/20.
+	 */
+	public class Spider
+	{
+
+	    public  static String sendGet(String url)
+	    {
+
+	        BufferedReader reader=null;
+
+	        String result="";
+
+	        try {
+	            URL realUrl = new URL(url);
+	            URLConnection connection = realUrl.openConnection();
+
+	            reader=new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
+
+	            String line;
+	            while ((line = reader.readLine()) != null) {
+	                result+=line;
+	            }
+
+	            return result;
+	        }catch (Exception e)
+	        {
+	            throw  new RuntimeException("get方法请求错误"+e);
+	        }finally {
+	            try {
+	                if (reader != null) reader.close();
+	            }catch (Exception e)
+	            {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    public static ArrayList<Zhihu> regexString(String targetStr)
+	    {
+	        ArrayList<Zhihu> lists=new ArrayList<>();
+
+
+
+	        Pattern urlPattern=Pattern.compile("<h2>.+?question_link.+?href=\"(.+?)\".+?</h2>");//匹配url地址
+	        Matcher urlMatcher=urlPattern.matcher(targetStr);
+
+	        while (urlMatcher.find())
+	        {
+	            Zhihu zhihu=new Zhihu("https://www.zhihu.com"+urlMatcher.group(1));
+
+
+	            lists.add(zhihu);
+	        }
+	        return lists;
+	    }
+	}
 	
 
 }
---
+++ F35A/src/domain/Zhihu.java
@@ -0,0 +1,84 @@
+package domain;
+
+import spider.Spider;
+
+import javax.swing.border.MatteBorder;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 创建bean对象，属性与我们需要的东西相配。
+ * Created by codingBoy on 16/10/20.
+ */
+public class Zhihu
+{
+    public String title;
+    public String titleDescription;
+    public String zhihuUrl;
+    public ArrayList<String> answers;
+
+
+
+    public Zhihu(String url)
+    {
+        title="";
+        titleDescription="";
+        zhihuUrl="";
+        answers=new ArrayList<>();
+
+        if (getRealUrl(url))
+        {
+            System.out.println("正在抓取链接"+zhihuUrl);
+
+            String content=Spider.sendGet(zhihuUrl);
+
+            Pattern pattern;
+            Matcher matcher;
+
+            pattern=Pattern.compile("zh-question-title.+?<h2.+?>(.+?)</h2>");
+            matcher=pattern.matcher(content);
+            if (matcher.find()) {
+                title = matcher.group(1);
+            }
+
+            pattern=Pattern.compile("zh-question-detail.+?<div.+?>(.*?)</div>");
+            matcher=pattern.matcher(content);
+            if (matcher.find()) {
+                titleDescription=matcher.group(1);
+            }
+
+            pattern=Pattern.compile("/answer/content.+?<div.+?>(.*?)</div>");
+            matcher=pattern.matcher(content);
+            while (matcher.find())
+            {
+                answers.add(matcher.group(1));
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "问题为:"+title+"\n问题描述为:"+titleDescription+
+                "\n地址为:"+zhihuUrl+"\n回答的内容为:"+answers+"\n";
+    }
+
+
+    boolean getRealUrl(String url)
+    {
+        String regex="question/(.+?)/";
+
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(url);
+
+        while (matcher.find())
+        {
+            zhihuUrl="https://www.zhihu.com/question/"+matcher.group(1);
+
+            return true;
+        }
+        return false;
+    }
+
+}
